8a7f36fe92c7b1ac2db50530ca8f3aac58c34105
prashant48653c <acharyaprashant227@gmail.com>
2026-05-13 10:53:40 +0545

    Print and Escape Sequence

--- joke.py
+++ joke.py
@@ -1,9 +1,16 @@
 from pyjokes import get_joke
 
+# This code uses the pyjokes library to fetch and print a random joke.
+'''
 def tell_joke():
     joke = get_joke()
     print(joke)
 
 
-print("Here's a joke for you:")
+print("Here\"s a joke for you:")
 tell_joke()
+
+'''
+
+print("What are you doing", "are you fine", sep="*", end="!!!\n")
+print("What are you doing", "are you fine", sep="*", end="!!!\n")
