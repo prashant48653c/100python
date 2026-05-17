18c60ce8594d04b0521def728298fadd3eaa1bf5
prashant48653c <acharyaprashant227@gmail.com>
2026-05-17 06:06:49 +0545

    Function argument ordering

--- function.py
+++ function.py
@@ -5,3 +5,14 @@ def sayHi(name="Tori", greeting="Hello"):
 
 name=input("Enter your name: ")
 sayHi(name=name, greeting="Hi")
+
+
+# This creates tuple of arguments and we can unpack it in the function definition
+def add(*nums):
+    total = 0
+    for num in nums:
+        total += num
+    return total
+
+
+print(add(1, 2, 3, 4, 5))
